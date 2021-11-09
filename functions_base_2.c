abe65255cefff0e0e3afaa9c6f8591cba866816c
MiguelBarreraDev <barreram987@gmail.com>
2021-11-09 16:50:26 -0500

    new restriction - line 27

--- functions_base_2.c
+++ functions_base_2.c
@@ -7,26 +7,27 @@
  */
 int digit_case_S(va_list ptr)
 {
-	int pos = 0, tmp = 0, len = 0, count = 0;
+	int pos = 0, tmp = 0, len = 0;
 	char *save = va_arg(ptr, char *);
 	char c;
 
 	while (*(save + tmp))
 	{
 		c = *(save + tmp);
-
 		if ((0 < c && 37 > c) || c >= 127)
 		{
-			count += _putchar('\\');
+			_putchar('\\');
 			pos++;
-			count += _putchar('x');
+			_putchar('x');
 			pos++;
 			len = convert_base(16, c, 2);
 			pos = pos + len;
 			c = *(save + tmp + 1);
 			tmp++;
+			if (c == '\0')
+				break;
 		}
-		count += _putchar(c);
+		_putchar(c);
 		pos++;
 		tmp++;
 	}
