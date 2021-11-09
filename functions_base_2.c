f62e40f55b4750327a3a7f5ba8e51bbeab6c4ccd
MiguelBarreraDev <barreram987@gmail.com>
2021-11-09 17:43:40 -0500

    new instancia line 25

--- functions_base_2.c
+++ functions_base_2.c
@@ -7,24 +7,23 @@
  */
 int digit_case_S(va_list ptr)
 {
-	int pos = 0, len = 0;
+	unsigned int pos = 0, len = 0;
 	char *save = va_arg(ptr, char *);
 
 	if (save == NULL)
 		save = "(null)";
 	while (*(save + pos))
 	{
-		if ((32 > *(save + pos)) || *(save + pos) >= 127)
+		if ((0 < *(save + pos) && 32 > *(save + pos)) || *(save + pos) >= 127)
 		{
 			_putchar('\\');
 			_putchar('x');
 			len += 2;
 			len += convert_base(16, *(save + pos), 2);
 			pos++;
-			if (*(save + pos) == '\0')
-				break;
 		}
-		len += _putchar(*(save + pos++));
+		else
+			len += _putchar(*(save + pos++));
 	}
 	return (len);
 }
