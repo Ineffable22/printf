12256f7f655ec03ade50c0ba8f9cdf3ccd9f4e3d
MiguelBarreraDev <barreram987@gmail.com>
2021-11-09 01:11:43 -0500

    The second half of the file is added

--- main.h
+++ main.h
@@ -17,3 +17,22 @@ int _printf(const char *format, ...);
  */
 typedef struct case_match
 {
+	char *c;
+	int (*f)(va_list);
+} c_mtc;
+/* ------------------------------------*/
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *);
+char *_strcpy(char *, char *);
+char *_strcat(char *, char *);
+/* --------------------------- */
+int char_case(va_list);
+int string_case(va_list);
+int digit_base_10(va_list);
+/*------------------------------------*/
+int restriction_percentage(const char *str);
+int (*match_case(const char *))(va_list);
+int convert_base(int base, int number, int band);
+
+#endif
