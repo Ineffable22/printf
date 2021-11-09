8aaaa16f2c2e2d1e143cc1ede461c63cfa7bf71f
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-09 20:27:30 +0000

    match.c update: new functions with flags

--- match.c
+++ match.c
@@ -5,7 +5,7 @@
  *
  * Return: Pointer to function associated to flag
  */
-int (*match_case(const char *flag))(va_list)
+int (*match_case(const char *flag))(va_list ptr)
 {
 	int pos = 0;
 
@@ -14,14 +14,19 @@ int (*match_case(const char *flag))(va_list)
 		{"c", char_case},
 		{"d", digit_base_10},
 		{"i", digit_base_10},
+		{"b", digit_base_2},
+		{"x", digit_base_16_lower},
+		{"X", digit_base_16_upper},
+		{"o", digit_base_8},
+		{"u", digit_case_u},
 		{NULL, NULL}
 	};
 
-	while (pos < 4)
+	while (pos < 9)
 	{
-		if (*flag == *(flag_store[pos]).c)
-			return (*(flag_store[pos]).f);
+		if (*(flag_store[pos]).c == *flag)
+			return (flag_store[pos].f);
 		pos++;
 	}
-	return (*(flag_store[pos]).f);
+	return (flag_store[pos].f);
 }
