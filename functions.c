baadc99f9d7ea3e314802230b435c0461650924c
MiguelBarreraDev <barreram987@gmail.com>
2021-11-09 14:59:16 -0500

    new functions print_number

--- functions.c
+++ functions.c
@@ -10,8 +10,7 @@
 int convert_base(int base, int number, int band)
 {
 	char buffer[17];
-	char buffersito[20];
-	char *ptr = &buffersito[20];
+	char *ptr;
 	long int save = number;
 
 	if (band == 0)
@@ -42,4 +41,33 @@ int convert_base(int base, int number, int band)
 	ptr++;
 
 	return (write(1, ptr, _strlen(ptr)));
+}
+
+
+/**
+ * print_number - prints an integer.
+ * @n: integer n to print using _putchar
+ *
+ * Return: none - void function
+ */
+int print_number(int n)
+{
+	unsigned int r;
+	int _length = 0;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		n *= -1;
+		_length++;
+	}
+	r = n;
+
+	if (r / 10)
+	{
+		_length += print_number(r / 10);
+	}
+	_putchar(r % 10 + '0');
+	_length++;
+	return (_length++);
 }
