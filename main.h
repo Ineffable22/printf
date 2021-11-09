e78184e097d589da4d017d8aa2d49638760315ee
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-09 19:58:18 +0000

    main.h update: new prototype

--- main.h
+++ main.h
@@ -34,5 +34,6 @@ int digit_base_10(va_list);
 int restriction_percentage(const char *str);
 int (*match_case(const char *))(va_list);
 int convert_base(int base, int number, int band);
+int print_number(int n);
 
 #endif
