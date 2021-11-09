0d970f7eb73af74b862b459cffa2c6178a0dfe05
MiguelBarreraDev <barreram987@gmail.com>
2021-11-09 01:16:39 -0500

    The second half of the file is added

--- printer.c
+++ printer.c
@@ -8,7 +8,7 @@
  */
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 /**
  * _puts - prints a string to stdout
@@ -16,13 +16,13 @@ int _putchar(char c)
  */
 void _puts(char *str)
 {
-        int x = 0;
+	int x = 0;
 
-        while (str[x] != 0)
-        {
+	while (str[x] != 0)
+	{
 		_putchar(str[x]);
-                x++;
-        }
+		x++;
+	}
 }
 /**
  * _strlen - returns the length of a string.
@@ -42,3 +42,47 @@ int _strlen(char *s)
 /**
  * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n, i;
+
+	n = 0;
+	while (src[n] != 0)
+	{
+		n++;
+	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x = 0;
+	int y = 0;
+
+	while (dest[x])
+	{
+		x++;
+	}
+	while (src[y])
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	return (dest);
+}
