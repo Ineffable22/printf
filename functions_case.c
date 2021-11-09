c084869ce09d9e6ccb1438dc2f01583fa2cad6a7
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-09 05:53:29 +0000

    Add: functions_case.c file

---
+++ functions_case.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * char_case - Add the argument character to the buffer
+ * @ptr: A variable that points to a list of arguments
+ *
+ * Return: length of string added to buffer
+ */
+int char_case(va_list ptr)
+{
+	char c = va_arg(ptr, int);
+	
+	if (c != '\0')
+		_putchar(c);
+	return (1);
+}
