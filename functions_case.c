f1064110f455d7ecddc501837dfc913ad8432a63
MiguelBarreraDev <barreram987@gmail.com>
2021-11-09 01:09:43 -0500

    The second half of the file is added

--- functions_case.c
+++ functions_case.c
@@ -8,8 +8,23 @@
 int char_case(va_list ptr)
 {
 	char c = va_arg(ptr, int);
-	
+
 	if (c != '\0')
 		_putchar(c);
 	return (1);
+}
+/**
+ * string_case - Add the argument string to the buffer
+ * @ptr: A variable that points to a list of arguments
+ *
+ * Return: length of string added to buffer
+ */
+int string_case(va_list ptr)
+{
+	char *save = va_arg(ptr, char *);
+
+	if (save == NULL)
+		save = "(null)";
+	_puts(save);
+	return (_strlen(save));
 }
