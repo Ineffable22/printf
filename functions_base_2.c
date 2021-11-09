2261627f9414bac6d25690f1d4a289a5402b44b0
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-09 23:10:29 +0000

    functions_base_2.c update: new prototype hex_print

--- functions_base_2.c
+++ functions_base_2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int hex_print(char c);
 /**
  * digit_case_S - custom conversion specifier
  * @ptr: A variable that points to a list of arguments
@@ -7,23 +8,47 @@
  */
 int digit_case_S(va_list ptr)
 {
-	int pos = 0, len = 0;
+	unsigned int pos = 0, len = 0;
 	char *save = va_arg(ptr, char *);
 
 	if (save == NULL)
 		save = "(null)";
 	while (*(save + pos))
 	{
-		if ((32 > *(save + pos)) || *(save + pos) >= 127)
+		if ((0 < *(save + pos) && 32 > *(save + pos)) || *(save + pos) >= 127)
 		{
 			_putchar('\\');
 			_putchar('x');
 			len += 2;
-			len += convert_base(16, *(save + pos), 2);
+			len += hex_print(*(save + pos));
 			pos++;
 		}
 		else
 			len += _putchar(*(save + pos++));
 	}
 	return (len);
+}
+
+/**
+ * hex_print - prints a char's ascii value in uppercase hex
+ * @c: char to print
+ *
+ * Return: number of chars printed (always 2)
+ */
+int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
 }
