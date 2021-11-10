d487dde2bf127cc5471cba1411ae5eeba3cc22ec
MiguelBarreraDev <barreram987@gmail.com>
2021-11-09 20:22:53 -0500

    new restriction line - 46

--- functions.c
+++ functions.c
@@ -19,15 +19,13 @@ int convert_base(int base, long int number, int band, char **add)
 		_strcpy(buffer, "0123456789ABCDEF");
 	else
 		_strcpy(buffer, "0123456789abcdef");
-	*ptr = '\0';
-	ptr--;
+	*ptr = '\0', ptr--;
 
 	if (number == 0)
 	{
 		**add = 48, (*add)++; /*_putchar(48);*/
 		return (1);
 	}
-	/* - - - - - - - - convert - - - - - - -  */
 	if (number < 0)
 		number = -number;
 	while (number > 0)
@@ -35,25 +33,27 @@ int convert_base(int base, long int number, int band, char **add)
 		*ptr-- = buffer[number % base];
 		number = number / base;
 	}
-	/* -- -  -- -- - - -- - - - -- - - - -- */
 	if (save < 0)
 		*ptr-- = '-';
 	if (band == 16)
 		*ptr-- = 'x', *ptr-- = '0';
 	ptr++;
-
 	if (_strlen(ptr) < 2 && band == 2)
-	{
-		**add = 0, (*add)++;
-		len_end++; /*_putchar('0');*/
-	}
+		**add = 0, (*add)++, len_end++; /*_putchar('0');*/
 	len_end += _strlen(ptr);
-	return (write(1, ptr, len_end));
+	while (len_end > 0)
+	{
+		**add = *ptr;
+		ptr++;
+		(*add)++;
+		len_end--;
+	}
+	return (0);
 }
 /**
  * print_number - prints an integer.
  * @n: integer n to print using _putchar
- * @add: A pointer pointing to a memory address within the buffer
+ * @add: A pointer pointing to a memory address within the buffer*
  *
  * Return: none - void function
  */
